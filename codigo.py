13bb976faefecd476fd4ad45f079210d562368db
IsaFernandes96 <133810582+IsaFernandes96@users.noreply.github.com>
2025-07-18 10:32:38 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -10,11 +10,11 @@ navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-
 
 navegador.maximize_window()
 navegador.find_element('xpath', 
-                       '//*[@id="firstname"]').send_keys("Edgar")
+                       '//*[@id="firstname"]').send_keys("Isabelle")
 navegador.find_element('xpath', 
-                       '//*[@id="email"]').send_keys("edgarlja@gmail.com")
+                       '//*[@id="email"]').send_keys("bebellefernandes96@gmail.com")
 navegador.find_element('xpath', 
-                       '//*[@id="phone"]').send_keys("11947498283")
+                       '//*[@id="phone"]').send_keys("11948849445")
 navegador.find_element('xpath', '//*[@id="_form_1925_submit"]').click()
 navegador.find_element('xpath', '//*[@id="botao-minicurso"]').click()
 navegador.maximize_window()
